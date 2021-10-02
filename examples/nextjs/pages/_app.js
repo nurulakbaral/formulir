0dff19a6c5d2b7085bd95cc92fa65f300b1c551d
nurulakbaral <nurulakbaral@gmail.com>
2021-10-02 07:10:15 +0700

    updates: nextjs (example project) #1

--- examples/nextjs/pages/_app.js
+++ examples/nextjs/pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider } from '@emotion/react'
+import { theme } from '../styles/theme'
+import { createEmotionCache } from '../src/utils/createEmotionCache'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+export default function MyApp(props) {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps,
+    } = props
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>My page</title>
+                <meta
+                    name='viewport'
+                    content='initial-scale=1, width=device-width'
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <Component {...pageProps} />
+                </LocalizationProvider>
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    emotionCache: PropTypes.object,
+    pageProps: PropTypes.object.isRequired,
+}
